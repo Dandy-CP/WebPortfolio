5b8553aebcf2e0d47cfd5ba84206c8ba15fb00db
Dandy Candra <dandycandrapratama@gmail.com>
2023-11-26 15:43:08 +0700

    add hooks auth context

---
+++ hooks/useAuth.js
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+
+import { AuthContext } from '../config/AuthProvider';
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+};
+
+export default useAuth;
